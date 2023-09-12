114c2158e1e72e82b1d1c77945ad39248e3ef78a
André Henrique Ludwig <127148504+AndreHLudwig@users.noreply.github.com>
2023-09-12 19:27:50 -0300

    nova att

---
+++ src/main/java/Abstrata/Principal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Abstrata;
+
+/**
+ *
+ * @author 06985933599
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+    }
+}
